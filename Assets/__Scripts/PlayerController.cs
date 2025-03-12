8091f1c05c3a8a7012e782c1aabc72c2584f243f
Orhan <orhantas3818@outlook.de>
2025-03-12 22:56:08 +0100

    e0.01 Prototype
    - added simple movement
    - added small level
    - added colliders
    - added Tilemaps

---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    // Einstellbare Bewegungsgeschwindigkeit
+    public float speed = 5f;
+
+    // Referenz auf das Rigidbody2D (für Physik-basierte Kollisionen)
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        // Hole das Rigidbody2D-Komponente des GameObjects
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    [System.Obsolete]
+    void Update()
+    {
+        // Abrufen der Eingaben ohne Glättung (sorgt für sofortiges Stoppen)
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        // Erstellen eines Vektors aus den Eingaben und Normalisieren, um konstante Geschwindigkeit zu gewährleisten
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+
+        // Setzen der Geschwindigkeit. Wenn keine Taste gedrückt wird, wird die Geschwindigkeit 0.
+        rb.velocity = movement * speed;
+    }
+}
